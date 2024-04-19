fab6be035e092dccd276f2d41913bbf42d21c00d
VishalKumarSain <vishaldhoria007@gmail.com>
2024-04-19 23:05:13 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        {/* Do not remove the main div */}
+        <input type="text" data-testid="input1" placeholder="Enter first name" />
+        <input type="text" data-testid="input2" placeholder="Enter second name" />
+        <button data-testid="calculate_relationship">Calculate Relationship Future</button>
+        <button data-testid="clear">Clear</button>
+        <h3 data-testid="answer"></h3>
+      </div>
+    );
+  }
 }
-
 
 export default App;
